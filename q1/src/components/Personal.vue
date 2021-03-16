d43274a72489c32c53134a73cfa27846001f97b6
Chadd Hodge <chadd.hodge0@gmail.com>
2021-03-16 19:36:26 -0400

    Component updates

--- q1/src/components/Personal.vue
+++ q1/src/components/Personal.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>This is personal</div>
+  <div class="tab-content">
+      <div class="row">
+          <div class="col">
+              <h5 class="tab-content-step">Step 2</h5>
+              <h3 class="tab-content-title">Your Personal information</h3>
+              <span class="tab-content-desc">Enter the login information for your account.
+                  You can change this information at any time</span>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+            <label for="fname">First Name</label>
+            <input id="fname" class="tab-container-input form-control">
+        </div>
+        <div class="col-6">
+            <label for="lname">Last Name</label>
+            <input id="lname" class="tab-container-input form-control">
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +27,6 @@ export default {
 }
 </script>
 
-<style scoped lang="sass">
+<style scoped lang="scss">
 
 </style>
--- q1/src/components/Tabs.vue
+++ q1/src/components/Tabs.vue
@@ -1,38 +1,80 @@
 <template>
 <div class="container-fluid">
     <div class="row">
-        <div class="tab-bar col">Basic</div>
-        <div class="tab-bar col">Personal</div>
-        <div class="tab-bar col">Prefs</div>
+        <div class="tab-bar tab-active col" @click="setTab(0)">Basic</div>
+        <div class="tab-bar col" @click="setTab(1)">Personal</div>
+        <div class="tab-bar col" @click="setTab(2)">Prefs</div>
     </div>
     <div class="row">
       <div class="tab-view-body">
-          <Basic></Basic>
+          <keep-alive>
+            <component :is="currentTab"></component>
+          </keep-alive>
       </div>
     </div>
     <div class="row">
-        <button class="col btn btn-primary">Back</button>
+        <button class="col btn btn-primary" @click="backTab">Back</button>
         <div class="col"></div>
-        <button class="col btn btn-primary">Next</button>
+        <button class="col btn btn-primary" @click="nextTab">Next</button>
     </div>
 </div>
 </template>
 
 <script>
 import Basic from './Basic.vue'
+import Personal from './Personal.vue'
+import Prefs from './Prefs.vue'
 export default {
-  components: { Basic },
+  components: { Basic, Personal, Prefs },
     name: 'Tabs',
+    data: () => ({
+        tabs: ['basic', 'personal', 'prefs'],
+        currentTab: '',
+        tabIndex: 0
+    }),
+    computed: {
+        // currentTab() {
+        //     return this.tabs[0];
+        // }
+    },
+    methods: {
+        setTab(index) {
+            this.tabIndex = index;
+            this.currentTab = this.tabs[index];
+        },
+        nextTab() {
+            if (this.tabs.length - 1 > this.tabIndex) {
+                this.tabIndex++
+                this.currentTab = this.tabs[this.tabIndex]
+            }
+        },
+        backTab() {
+            if (this.tabIndex > 0) {
+                this.tabIndex--
+                this.currentTab = this.tabs[this.tabIndex]
+            }
+        }
+    },
+    mounted() {
+        this.currentTab = this.tabs[this.tabIndex];
+    }
 }
 </script>
 
 <style scoped lang="scss">
 
+$activeColor: blue;
+
     .tab {
         &-bar {
             padding: 20px;
             border: 1px solid black;
-        }  
+        } 
+
+        &-active {
+            color: $activeColor;
+            font-weight: bold;
+        } 
 
         &-view-body {
             padding: 20px 40px 30px 40px;
